a551c33eb57a6ddef4d7968c1531cf753566d22b
Gary Andelin <gary.l.andelin@boeing.com>
2017-06-08 15:43:02 -0700

    modified to with with templates

--- js/manageContacts.js
+++ js/manageContacts.js
@@ -3,72 +3,49 @@ var nextId = 1000;
 var contacts = getContacts();
 var contactsVisible = false;
 
+$('#addNewItem').on('click', addNewContact);
+
+var contactFormTemplate = Handlebars.compile($('#contactFormTemplate').html());
+var contactsTableTemplate = Handlebars.compile($('#contactsTableTemplate').html());
+
 $(document).ready(function() {
   if (contacts.length > 0) {
     updateContactTable();
     resetForm();
-    togglePageVisibility();
   }
-});
-
-$("#cancel").click(function() {
   togglePageVisibility();
 });
 
-$("#addNewItem").on('click', addNewContact);
-$('#contact-tbody').on('click', '.edit', editContact);
-$('#contact-tbody').on('click', '.delete', deleteContact);
+function updateContactTable() {
+  var data = { contacts: contacts};
+  var html = contactsTableTemplate(data);
+  $('#contactsTableDiv').html(html);
+  
+  $('#contact-tbody').on('click', '.edit', editContact);
+  $('#contact-tbody').on('click', '.delete', deleteContact);
+}
 
 function resetForm() {
   $('#name').val("");
   $('#phone').val("");
 }
 
-function updateContactTable() {
-  for (var i=0; i<contacts.length; i++) {
-    console.log(contacts[i].name);
-    console.log(contacts[i].phone);
-  }
-  var tbody = $("#contact-tbody");
-  tbody.empty();
-  contacts.forEach(function(contact) {
-    var contactRow = createTableRow(contact);
-    tbody.append(contactRow);
-  });
-}
-
-function createTableRow(contact) {
-  var tableRow = $('<tr data-id="' + contact.id + '">');
-  var contactName = $('<td>').text(contact.name);
-  tableRow.append(contactName);
-  var contactPhone = $('<td>').text(contact.phone);
-  tableRow.append(contactPhone);
-  var td = $('<td>');
-  var button = $('<td>').text
-  var button = $('<button type="button" class="edit">');
-  button.text("Edit");
-  td.append(button);
-  tableRow.append(td);
-  td = $('<td>');
-  button = $('<button type="button" class="delete">');
-  button.text("Delete");
-  td.append(button);
-  tableRow.append(td);
-  return tableRow;
-}
-
 function togglePageVisibility() {
   if (contactsVisible) {
     // hide contacts, show form
-    $("#contactEntry").removeClass('hide');
-    $("#contacts").addClass('hide');
+    $("#contactFormDiv").removeClass('hide');
+    $("#contactsTableDiv").addClass('hide');
   }
   else {
     // show contacts, hide form
-    $("#contacts").removeClass('hide');
-    $("#contactEntry").addClass('hide');
+    $("#contactsTableDiv").removeClass('hide');
+    $("#contactFormDiv").addClass('hide');
   }
   contactsVisible = !contactsVisible;
+}
+
+function removeForm() {
+  $('#contactFormDiv').empty();
 }
 
 function addNewContact() {
@@ -106,16 +83,16 @@ function deleteContact(e) {
 }
 
 function createOrEditContact(contact) {
+  var data = { name: '', phone: ''};
   if (contact) {
-    $('#name').val(contact.name);
-    $('#phone').val(contact.phone);
+    data = { name: contact.name,
+             phone: contact.phone};
   }
-  else {
-    $('#name').val('');
-    $('#phone').val('');
-  }
+  var html = contactFormTemplate(data);
+  $('#contactFormDiv').html(html);
   
-  $('#submit').one('click', createOrUpdateContact);
+  $('#submitContact').one('click', createOrUpdateContact);
+  $('#cancelContact').one('click', togglePageVisibility);
   
   togglePageVisibility();
   
@@ -153,3 +130,4 @@ function getContacts() {
 function saveContacts() {
   localStorage[CONTACTS_PERSISTENCE_KEY] = JSON.stringify(contacts);
 }
+
